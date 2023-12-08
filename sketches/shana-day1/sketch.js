b948226b6340330cbbd63b204ce0d1c9b1be802b
Sheloux <sheloux13@hotmail.fr>
2023-12-08 13:32:13 +0100

    Update sketch.js

--- sketches/shana-day1/sketch.js
+++ sketches/shana-day1/sketch.js
@@ -5,6 +5,12 @@ const gridCount = 5
 let soundEffect;
 let soundPlayed = false;
 let mousePressed = false;
+let started = false;
+const currentScale = new SpringNumber({
+    position: 100, // start position
+    frequency: 2, // oscillations per second (approximate)
+    halfLife: 0.15 // time until amplitude is halved
+})
 import { SpringNumber } from "../../shared/spring.js"
 
 
@@ -22,6 +28,9 @@ window.preload = function () {
 }
 window.mouseClicked = function () {
     mousePressed = true;
+    started = true;
+    currentScale.target = 1;
+
 }
 
 window.setup = function () {
@@ -40,6 +49,13 @@ window.setup = function () {
 
         }
     }
+    const sceneSize = min(width, height)
+    const objSize = sceneSize / 2
+    const rectSize = objSize / (gridCount - 1)
+    let endObjSize = rectSize + objSize;
+    currentScale.position = objSize / endObjSize;
+    currentScale.target = currentScale.position;
+
 
     angleMode(DEGREES)
 }
@@ -58,50 +74,75 @@ window.draw = function () {
     const centerY = height / 2
     const objSize = sceneSize / 2
     const strokeW = 20
-
-
-    fill(0)
-    noStroke()
-    const pointSize = strokeW
-
-    rectMode(CENTER)
-
-    const allSquaresActivated = gridObjects.every(obj => obj.circleActive);
     let rectSize = objSize / (gridCount - 1)
 
-    spring.target = rectSize;
+    currentScale.step(deltaTime / 1000);
 
-    if (allSquaresActivated) {
-        // console.log("YEY", spring.position);
-        // spring.position = realRectSize;
-        spring.target = pointSize;
 
-        spring.step(deltaTime / 1000);
+    translate(width / 2, height / 2);
+    scale(currentScale.position);
+
+    const allSquaresActivated = gridObjects.every(obj => obj.circleActive);
+
+    if (started) {
+        fill(0)
+        noStroke()
+        const pointSize = strokeW
+
+        rectMode(CENTER)
+
+
+        spring.target = rectSize;
+
+        if (allSquaresActivated) {
+            // console.log("YEY", spring.position);
+            // spring.position = realRectSize;
+            spring.target = pointSize;
+
+            spring.step(deltaTime / 1000);
+        }
+
+        for (const obj of gridObjects) {
+
+            const x = obj.gridX;
+            const y = obj.gridY;
+            const xPos = map(x, 0, gridCount - 1, centerX - objSize / 2, centerX + objSize / 2)
+            const yPos = map(y, 0, gridCount - 1, centerY - objSize / 2, centerY + objSize / 2)
+
+
+            if (mousePressed &&
+                mouseX > xPos - rectSize / 2 && mouseX < xPos + rectSize / 2 &&
+                mouseY > yPos - rectSize / 2 && mouseY < yPos + rectSize / 2) {
+                obj.circleActive = true
+                soundEffect.play();
+                mousePressed = false
+            }
+
+            const transitionTime = 0.1; // seconds
+            if (obj.circleActive) {
+                obj.circleProgress += deltaTime / 1000 / transitionTime;
+                obj.circleProgress = constrain(obj.circleProgress, 0, 1)
+            }
+
+
+        }
+        if (abs(spring.position - spring.target) <= 0.1) {
+            sendSequenceNextSignal(); // finish sketch
+            noLoop();
+        }
     }
+    rectMode(CENTER)
 
     for (const obj of gridObjects) {
 
         const x = obj.gridX;
         const y = obj.gridY;
-        const xPos = map(x, 0, gridCount - 1, centerX - objSize / 2, centerX + objSize / 2)
-        const yPos = map(y, 0, gridCount - 1, centerY - objSize / 2, centerY + objSize / 2)
+        const xPos = map(x, 0, gridCount - 1, - objSize / 2, + objSize / 2)
+        const yPos = map(y, 0, gridCount - 1, - objSize / 2, + objSize / 2)
 
-
-        if (mousePressed &&
-            mouseX > xPos - rectSize / 2 && mouseX < xPos + rectSize / 2 &&
-            mouseY > yPos - rectSize / 2 && mouseY < yPos + rectSize / 2) {
-            obj.circleActive = true
-            soundEffect.play();
-            mousePressed = false
-        }
-
-        const transitionTime = 0.1; // seconds
-        if (obj.circleActive) {
-            obj.circleProgress += deltaTime / 1000 / transitionTime;
-            obj.circleProgress = constrain(obj.circleProgress, 0, 1)
-        }
 
         fill(0)
+
         const rectRadius = map(obj.circleProgress, 0, 1, 0, rectSize / 2)
         const realRectSize = rectSize; // map(obj.circleProgress, 0, 1, rectSize, pointSize);
 
@@ -112,10 +153,9 @@ window.draw = function () {
         } else {
             rect(xPos, yPos, realRectSize + 1, realRectSize + 1, rectRadius)
         }
+
     }
-    if (spring.position == spring.target) {
-        sendSequenceNextSignal(); // finish sketch
-        noLoop();
-    }
+    fill(0, 255, 0)
+    // circle(centerX, centerY, 100);
 
 }
